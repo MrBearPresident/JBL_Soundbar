4749b55d0c25713cbbf034cfc4576a10bcc3fab6
almirus <almir.abrarov@gmail.com>
2026-04-18 11:45:27 +0300

    Add Audio Format sensor

--- custom_components/jbl_integration/sensor.py
+++ custom_components/jbl_integration/sensor.py
@@ -35,6 +35,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
     entityArray.append(JBLSensor(coordinator,entry,"track","Track","mdi:information"))
     entityArray.append(JBLSensor(coordinator,entry,"channel","Channel","mdi:information"))
     entityArray.append(JBLSensor(coordinator,entry,"source","Source","mdi:import"))
+    entityArray.append(JBLSensor(coordinator,entry,"audio_format","Audio Format","mdi:surround-sound"))
     
     if "Rears" in coordinator.data:
         entityArray.append(JBLRearSensor(coordinator,entry,0))
